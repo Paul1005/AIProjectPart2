1358d5990195aa9b74dc31bc88de10f7de00bb59
Paul1005 <mccarliep@gmail.com>
2019-10-15 00:28:16 -0700

    made some more progress

--- Assignment2/Assignment2/Assignment2.cpp
+++ Assignment2/Assignment2/Assignment2.cpp
@@ -5,11 +5,12 @@
 #include <stdlib.h>
 #include <stack> 
 #include "Cell.h"
+#include <vector>
 using namespace std;
 
 Cell grid[10][10];
-stack <pair<int, int>> openList;
-stack <pair<int, int>> closedList;
+vector<Cell> openList;
+vector<Cell> closedList;
 
 pair<int, int> blockedCells[22] = { {7, 1},
 {2, 2}, {3, 2}, {4, 2}, {5, 2}, {5, 2}, {7, 2},
@@ -66,14 +67,63 @@ void printArea() {
 }
 
 void findShortestPath() {
-	/*int h = (cell.x - goal.x) + (cell.y - goal.y);
-	int g = distancetravelled;
-	int f = h + g;*/
-
-	openList.push(make_pair(0,0));
+	Cell startCell(0, 0);
+	startCell.g = 0;
+	Cell endCell(9, 9);
+	startCell.h = heuristic(startCell, endCell);
+	startCell.f = startCell.h + startCell.g;
+	openList.push_back(startCell);
 	while (openList.size() > 0) {
+		Cell currentCell = openList.at(0);
+		for (int i = 1; i < openList.size(); i++)
+		{
+			if (currentCell.f > openList.at(i).f) {
+				currentCell = openList.at(i); 
+			}
+		}
 
+		if (currentCell.col == endCell.col && currentCell.row == endCell.row) {
+			tracePath();
+		}
+
+		openList.pop_back();
+		closedList.push_back(currentCell);
+
+		vector<Cell> neigbours;
+		int col = currentCell.col;
+		int row = currentCell.row;
+
+		neigbours.push_back(grid[col-1][row]);
+		neigbours.push_back(grid[col][row-1]);
+		neigbours.push_back(grid[col+1][row]);
+		neigbours.push_back(grid[col][row+1]);
+
+		for (Cell neighbour : neigbours)
+		{
+			bool isInClosedSet = false;
+			for (Cell cellInClosedList: closedList) {
+				if (neighbour.col == cellInClosedList.col && neighbour.row == neighbour.row) {
+					isInClosedSet = true;
+					break;
+				}
+			}
+
+			if (!isInClosedSet) {
+				break;
+			}
+
+			int score = currentCell.g + 1;
+
+			if (score < neighbour.g) {
+
+			}
+		}
 	}
+}
+
+int heuristic(Cell cell, Cell endCell) {
+	int h = (cell.col - endCell.col) + (cell.row - endCell.row);
+	return h;
 }
 
 int main()
@@ -82,5 +132,5 @@ int main()
 
 	printArea();
 
-	//findShortestPath();
+	findShortestPath();
 }
