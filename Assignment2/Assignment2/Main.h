a8eb0906d9a67a7a55fbf9bf4b7e016bb77266a5
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2019-11-04 16:20:29 -0800

    Split some more OpenCL stuff out of Main.h.

--- Assignment2/Assignment2/Main.h
+++ Assignment2/Assignment2/Main.h
@@ -13,11 +13,7 @@
 
 #ifdef __APPLE__
 #include <sys/time.h>
-#include <OpenCL/cl.h>
 #else
-#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
-#include <CL/cl.h>
-
 #include <Windows.h>
 
 /* FILETIME of Jan 1 1970 00:00:00. */
@@ -55,10 +51,3 @@ private:
 	struct timeval tvBegin, tvEnd, tvDiff;
 
 };
-
-bool CheckOpenCLError(cl_int errNum, const char* errMsg);
-cl_context CreateContext();
-cl_command_queue CreateCommandQueue(cl_context context, cl_device_id* device);
-cl_program CreateProgram(cl_context context, cl_device_id device, const char* fileName);
-bool CreateMemObjects(cl_context context, cl_mem memObjects[3], float* a, float* b);
-void Cleanup(cl_context context, cl_command_queue commandQueue, cl_program program, cl_kernel kernel, cl_mem memObjects[3]);
--- Assignment2/Assignment2/OpenCL.h
+++ Assignment2/Assignment2/OpenCL.h
@@ -1,3 +1,18 @@
 #pragma once
 
+
+#ifdef __APPLE__
+#include <OpenCL/cl.h>
+#else
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#include <CL/cl.h>
+#endif
+
 int OpenCLDemo();
+
+bool CheckOpenCLError(cl_int errNum, const char* errMsg);
+cl_context CreateContext();
+cl_command_queue CreateCommandQueue(cl_context context, cl_device_id* device);
+cl_program CreateProgram(cl_context context, cl_device_id device, const char* fileName);
+bool CreateMemObjects(cl_context context, cl_mem memObjects[3], float* a, float* b);
+void Cleanup(cl_context context, cl_command_queue commandQueue, cl_program program, cl_kernel kernel, cl_mem memObjects[3]);
